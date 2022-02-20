463100f090ea3214658f8e70a167cb0782f9d9a8
mikeploythai <54593375+mikeploythai@users.noreply.github.com>
2022-02-19 17:58:44 -0800

    Experimenting with React

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,14 +1,25 @@
-import './ExploreContainer.css';
+import "./ExploreContainer.css";
 
-interface ContainerProps { }
+interface ContainerProps {
+	name?: string;
+}
 
-const ExploreContainer: React.FC<ContainerProps> = () => {
-  return (
-    <div className="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
-  );
+const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+	return (
+		<div className="container">
+			<strong>{name}</strong>
+			<p>
+				Start with Ionic{" "}
+				<a
+					target="_blank"
+					rel="noopener noreferrer"
+					href="https://ionicframework.com/docs/components"
+				>
+					UI Components
+				</a>
+			</p>
+		</div>
+	);
 };
 
 export default ExploreContainer;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,58 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+	IonContent,
+	IonHeader,
+	IonPage,
+	IonTitle,
+	IonToolbar,
+	IonFab,
+	IonFabButton,
+	IonIcon,
+	IonGrid,
+	IonRow,
+	IonCol,
+	IonImg,
+	IonActionSheet,
+} from "@ionic/react";
+import "./Home.css";
+import { camera } from "ionicons/icons";
+import { usePhotoGallery } from "../hooks/usePhotoGallery";
 
 const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
+	const { photos, takePhoto } = usePhotoGallery();
+
+	return (
+		<IonPage>
+			<IonHeader>
+				<IonToolbar>
+					<IonTitle>Ionic Gallery</IonTitle>
+				</IonToolbar>
+			</IonHeader>
+
+			<IonContent fullscreen>
+				<IonHeader collapse="condense">
+					<IonToolbar>
+						<IonTitle size="large">Ionic Gallery</IonTitle>
+					</IonToolbar>
+				</IonHeader>
+
+				<IonGrid>
+					<IonRow>
+						{photos.map((photo, index) => (
+							<IonCol size="12" size-md="3" key={index}>
+								<IonImg src={photo.webviewPath} />
+							</IonCol>
+						))}
+					</IonRow>
+				</IonGrid>
+
+				<IonFab vertical="bottom" horizontal="center" slot="fixed">
+					<IonFabButton onClick={() => takePhoto()}>
+						<IonIcon icon={camera}></IonIcon>
+					</IonFabButton>
+				</IonFab>
+			</IonContent>
+		</IonPage>
+	);
 };
 
 export default Home;
